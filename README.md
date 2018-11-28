fd012844f010868cca0437aa22c3741b6aa3a365
ImGabe <gabrielpmonte@hotmail.com>
2018-11-28 23:11:38 +0000

    energia

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Tamagochi
 
+[![Build Status](https://travis-ci.com/ImGabe/Tamagochi.svg?branch=master)](https://travis-ci.com/ImGabe/Tamagochi)
--- testpy.py
+++ testpy.py
@@ -2,22 +2,35 @@ class Tamagochi:
   def __init__(self):
     self.nome = 'sem_nome'
     self.nivel = 0
+    self.energia = 100
 
+  # nome
   def mostrar_nome(self):
     print(f"O nome do seu tamagochi é {self.nome}.")
 
+  # nivel
   def mostrar_nivel(self):
     print(f"Nivel: {self.nivel}")
 
   def subir_nivel(self):
-  	self.nivel += 1
-  	print("")
+    self.nivel += 1
+    print(f"{self.nome} upou para o nível {self.nivel}")
+
+  # energia
+  def mostrar_energia(self):
+    print(f"Energia: {self.energia}")
+
+  def mostrar_profile(self):
+    self.mostrar_nome()
+    self.mostrar_nivel()
+    self.mostrar_energia()
 
   def mostrar_status(self):
-  	self.mostrar_nome()
-  	self.mostrar_nivel()
+    self.mostrar_nivel()
+    self.mostrar_energia()
 
+# inicia 
 if __name__ == '__main__':
-	tamagochi = Tamagochi()
-	tamagochi.nome = "Null"
-	tamagochi.mostrar_status()
+  tamagochi = Tamagochi()
+  tamagochi.nome = "Jabe"
+  tamagochi.mostrar_profile()
