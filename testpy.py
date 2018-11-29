ba794d1326e2f5e7daac251959d13dfa15ea6a61
ImGabe <gabrielpmonte@hotmail.com>
2018-11-28 22:26:43 -0200

    vida

--- testpy.py
+++ testpy.py
@@ -1,8 +1,14 @@
+from random import randint
+
 class Tamagochi:
   def __init__(self):
     self.nome = 'sem_nome'
     self.nivel = 0
+    self.fome = 0
+    self.vida = 100
     self.energia = 100
+    self.felicidade = 100
+    self.vivo = True
 
   # nome
   def mostrar_nome(self):
@@ -16,20 +22,71 @@ class Tamagochi:
     self.nivel += 1
     print(f"{self.nome} upou para o nível {self.nivel}")
 
+  # fome
+  def mostrar_fome(self):
+    print(f"Fome: {self.fome}")
+
+  def subir_fome(self):
+    n = randint(1, 10)
+    self.fome += n
+
+  def descer_fome(self):
+    n = randint(1, 10)
+    self.fome -= n
+
+  # vida 
+  def mostrar_vida(self):
+    print(f"Vida: {self.vida}")
+
+  def subir_vida(self):
+    n = randint(1, 10)
+    self.vida += n
+
+  def descer_vida(self):
+    n = randint(1, 10)
+    self.vida -= n
+
   # energia
   def mostrar_energia(self):
     print(f"Energia: {self.energia}")
 
+  def subir_energia(self):
+    n = randint(1, 10)
+    self.energia += n
+
+  def descer_energia(self):
+    n = randint(1, 10)
+    self.energia -= n
+
+  # felicidade
+  def mostrar_felicidade(self):
+    print(f"Felicidade: {self.felicidade}")
+
+  def subir_felicidade(self):
+    n = randint(1, 10)
+    self.felicidade += n
+
+  def descer_felicidade(self):
+    n = randint(1, 10)
+    self.felicidade -= n
+
+  # Profile e Status
   def mostrar_profile(self):
     self.mostrar_nome()
     self.mostrar_nivel()
+    self.mostrar_fome()
+    self.mostrar_vida()
     self.mostrar_energia()
+    self.mostrar_felicidade()
 
   def mostrar_status(self):
     self.mostrar_nivel()
+    self.mostrar_fome()
+    self.mostrar_vida()
     self.mostrar_energia()
+    self.mostrar_felicidade()
 
-# inicia 
+# inicia o projeto
 if __name__ == '__main__':
   tamagochi = Tamagochi()
   tamagochi.nome = "Jabe"
