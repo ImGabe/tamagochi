40fa67380d7887f89c396d4127e0552e62426673
Gabe <gabrielpmonte@hotmail.com>
2018-12-10 09:09:42 -0200

    🔧 fix

--- main.py
+++ main.py
@@ -1,118 +1,120 @@
 #coding: utf-8
 from random import randint
 
+
 class Tamagochi:
-  def __init__(self):
-    self.nome = 'sem_nome'
-    self.nivel = 0
-    self.fome = 0
-    self.vida = 100
-    self.energia = 100
-    self.felicidade = 100
-    self.vivo = True
+    def __init__(self):
+        self.nome = 'sem_nome'
+        self.nivel = 0
+        self.fome = 0
+        self.vida = 100
+        self.energia = 100
+        self.felicidade = 100
+        self.vivo = True
 
-  # printar
-  def printar(self, metodo):
-    if callable(metodo): 
-      print(metodo())
-    else: 
-      print(metodo)
+    # printar
+    def printar(self, metodo):
+        if callable(metodo):
+            print(metodo())
+        else:
+            print(metodo)
 
-  # verificar nome
-  def verificar_nome(self, nome):
-    while self.nome == "":
-      self.nome = input("Nome: ").strip()
-    else:
-      pass
+    # verificar nome
+    def verificar_nome(self, nome):
+        while self.nome == "":
+            self.nome = input("Nome: ").strip()
+        else:
+            pass
 
-  # Eventos
+    # Eventos
 
-  # gerador de evento
-  def funcname(self):
-    pass
+    # gerador de evento
+    def funcname(self):
+        pass
 
-  # nome
-  def set_nome(self, name):
-    return f"O nome do seu tamagochi é {self.nome}."
+    # nome
+    def set_nome(self, name):
+        return f"O nome do seu tamagochi é {self.nome}."
 
-  # nivel
-  def set_nivel(self):
-    return f"Nivel: {self.nivel}"
+    # nivel
+    def set_nivel(self):
+        return f"Nivel: {self.nivel}"
 
-  def subir_nivel(self):
-    self.nivel += 1
-    print(f"{self.nome} upou para o nível {self.nivel}")
+    def subir_nivel(self):
+        self.nivel += 1
+        print(f"{self.nome} upou para o nível {self.nivel}")
 
-  # vida 
-  def set_vida(self):
-    return f"Vida: {self.vida}"
+    # vida
+    def set_vida(self):
+        return f"Vida: {self.vida}"
 
-  def subir_vida(self):
-    n = randint(1, 10)
-    self.vida += n
+    def subir_vida(self):
+        n = randint(1, 10)
+        self.vida += n
 
-  def descer_vida(self):
-    n = randint(1, 10)
-    self.vida -= n
+    def descer_vida(self):
+        n = randint(1, 10)
+        self.vida -= n
 
-  # fome
-  def set_fome(self):
-    return f"Fome: {self.fome}"
+    # fome
+    def set_fome(self):
+        return f"Fome: {self.fome}"
 
-  def subir_fome(self):
-    n = randint(1, 10)
-    self.fome += n
+    def subir_fome(self):
+        n = randint(1, 10)
+        self.fome += n
 
-  def descer_fome(self):
-    n = randint(1, 10)
-    self.fome -= n
-    
-  # energia
-  def set_energia(self):
-    return f"Energia: {self.energia}"
+    def descer_fome(self):
+        n = randint(1, 10)
+        self.fome -= n
 
-  def subir_energia(self):
-    n = randint(1, 10)
-    self.energia += n
+    # energia
+    def set_energia(self):
+        return f"Energia: {self.energia}"
 
-  def descer_energia(self):
-    n = randint(1, 10)
-    self.energia -= n
+    def subir_energia(self):
+        n = randint(1, 10)
+        self.energia += n
 
-  # felicidade
-  def set_felicidade(self):
-    return f"Felicidade: {self.felicidade}"
+    def descer_energia(self):
+        n = randint(1, 10)
+        self.energia -= n
 
-  def subir_felicidade(self):
-    n = randint(1, 10)
-    self.felicidade += n
+    # felicidade
+    def set_felicidade(self):
+        return f"Felicidade: {self.felicidade}"
 
-  def descer_felicidade(self):
-    n = randint(1, 10)
-    self.felicidade -= n
+    def subir_felicidade(self):
+        n = randint(1, 10)
+        self.felicidade += n
 
-  # Profile e Status
+    def descer_felicidade(self):
+        n = randint(1, 10)
+        self.felicidade -= n
 
-  def mostrar_status(self):
-    self.printar(f"Nivel: {self.nivel}")
-    self.printar(f"Vida: {self.vida}")
-    self.printar(f"Fome: {self.fome}")
-    self.printar(f"Energia: {self.energia}")
-    self.printar(f"Felicidade: {self.felicidade}")
+    # Profile e Status
 
-  def mostrar_profile(self):
-    self.printar(f"O nome do seu tamagochi é {self.nome}.")
-    self.mostrar_status()
+    def mostrar_status(self):
+        self.printar(f"Nivel: {self.nivel}")
+        self.printar(f"Vida: {self.vida}")
+        self.printar(f"Fome: {self.fome}")
+        self.printar(f"Energia: {self.energia}")
+        self.printar(f"Felicidade: {self.felicidade}")
 
-  # Ações
-  def brincar(self):
-    self.subir_felicidade()
-    self.subir_fome()
-    self.descer_energia()
+    def mostrar_profile(self):
+        self.printar(f"O nome do seu tamagochi é {self.nome}.")
+        self.mostrar_status()
+
+    # Ações
+    def brincar(self):
+        self.subir_felicidade()
+        self.subir_fome()
+        self.descer_energia()
+
 
 # inicia o projeto
 if __name__ == '__main__':
-  tamagochi = Tamagochi()
-  tamagochi.nome = input("Nome: ").strip()
-  tamagochi.verificar_nome(tamagochi.nome)
-  tamagochi.mostrar_profile()
+    tamagochi = Tamagochi()
+    tamagochi.nome = input("Nome: ").strip()
+    tamagochi.verificar_nome(tamagochi.nome)
+    tamagochi.mostrar_profile()
--- tamagochidb.py
+++ tamagochidb.py
@@ -2,12 +2,15 @@ import sqlite3
 import os
 
 # Função para criar um pasta
+
+
 def createFolder(directory):
     try:
         if not os.path.exists(directory):
             os.makedirs(directory)
     except OSError:
-        print ('Error: Creating directory. ' +  directory)
+        print('Error: Creating directory. ' + directory)
+
 
 # Cria uma pasta chamada "database".
 createFolder('./database/')
@@ -16,11 +19,14 @@ createFolder('./database/')
 conn = sqlite3.connect('./database/tamagochidb.db')
 cursor = conn.cursor()
 # Cria uma tabela toda errada se não existir.
-cursor.execute('CREATE TABLE IF NOT EXISTS  tamagochi (ID INT PRIMARY KEY UNIQUE, Nome VARCHAR (255) UNIQUE NOT NULL, lvl INT NOT NULL)')
+cursor.execute(
+    'CREATE TABLE IF NOT EXISTS  tamagochi (ID INT PRIMARY KEY UNIQUE, Nome VARCHAR (255) UNIQUE NOT NULL, lvl INT NOT NULL)')
 
 # Função para adicionar um tamagochi na tabela
+
+
 def adicionar(nome, lvl):
-    cursor.execute("INSERT INTO tamagochi (Nome, Lvl) VALUES (?,?)", (nome,lvl))
+    cursor.execute(
+        "INSERT INTO tamagochi (Nome, Lvl) VALUES (?,?)", (nome, lvl))
     conn.commit()
     conn.close()
-
